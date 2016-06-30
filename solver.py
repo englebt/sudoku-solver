d4d5c39912ec5e5f7ef8d66c4d1ee529bd9a49c2
Brian Engle <englebt@gmail.com>
2016-06-30 16:55:19 -0400

    small updates.

--- solver.py
+++ solver.py
@@ -16,9 +16,6 @@ units = dict((s, [u for u in unitlist if s in u])
 peers = dict((s, set(sum(units[s],[]))-set([s]))
              for s in squares)
 
-grid1  = '003020600900305001001806400008102900700000008006708200002609500800203009005010300'
-grid2  = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
-
 ########## Parsing the Grid ##########
 
 def parse_grid(grid):
